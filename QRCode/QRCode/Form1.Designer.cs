8c1db2acf8803018dc3dfcbf553cb62ee9a8c9a9
CSUNKING <csunking@gmail.com>
2017-02-10 16:49:17 +0800

    二维码生成
    便于查看网络版本的操作说明文件

---
+++ QRCode/QRCode/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace QRCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ptQRCode = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptQRCode)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ptQRCode
+            // 
+            this.ptQRCode.Location = new System.Drawing.Point(6, 20);
+            this.ptQRCode.Name = "ptQRCode";
+            this.ptQRCode.Size = new System.Drawing.Size(264, 247);
+            this.ptQRCode.TabIndex = 0;
+            this.ptQRCode.TabStop = false;
+            this.ptQRCode.Paint += new System.Windows.Forms.PaintEventHandler(this.ptQRCode_Paint);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.ptQRCode);
+            this.groupBox1.Controls.Add(this.btnCreate);
+            this.groupBox1.Location = new System.Drawing.Point(264, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 296);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "二维码预览";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(159, 273);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(48, 273);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 3;
+            this.btnCreate.Text = "生成";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Controls.Add(this.txtContent);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(552, 325);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "信息";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtContent.Location = new System.Drawing.Point(3, 17);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(255, 296);
+            this.txtContent.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 325);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "二维码生成器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptQRCode)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptQRCode;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtContent;
+    }
+}
+
---
+++ QRCode/QRCode/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QRCode
+{
+    public partial class Form1 : Form
+    {
+        DotNetBarcode bc = new DotNetBarcode();
+        Image printImage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            this.ptQRCode.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bc.Type = DotNetBarcode.Types.QRCode;
+            bc.PrintCheckDigitChar = true;
+        }
+
+        private void ptQRCode_Paint(object sender, PaintEventArgs e)
+        {
+            bc.WriteBar(this.txtContent.Text.Trim(), 0, 0, this.ptQRCode.Width, this.ptQRCode.Height, e.Graphics);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string url = null;
+            Bitmap b = new Bitmap(ptQRCode.Width, ptQRCode.Height);
+            ptQRCode.DrawToBitmap(b,new Rectangle(0,0,ptQRCode.Width,ptQRCode.Height));
+            SaveFileDialog sfd=new SaveFileDialog ();
+            sfd.InitialDirectory=Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            sfd.Filter="jpg图片|*.jpg|bmp图片|*.bmp";
+            sfd.RestoreDirectory=true;
+            sfd.Title="请选择要保存的位置";
+            if(sfd.ShowDialog()==DialogResult.OK)
+            {
+            url=sfd.FileName;
+            }
+            printImage=b;
+            if(url!=null)
+            {
+            b.Save(url);
+            }
+
+
+
+        }
+    }
+}
